8782ac6b9c01c8afd4c13376c3963c24ce28da66
AnilAttada <anilattada222@gmail.com>
2026-05-08 21:19:11 +0530

    ROLES TF

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
 
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
-    alb_listener_arn = "${var.component}" == "frontend" ? "local.frontend_alb_listener_arn" : "local.backend_alb_listener_arn"
+    alb_listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
 
     common_tags = {
         Project = var.project
