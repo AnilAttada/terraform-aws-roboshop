db4e80475d4e62c2f4d4977786ff061dff3bac27
AnilAttada <anilattada222@gmail.com>
2026-05-08 20:46:55 +0530

    ROLES TF

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_autoscaling_group" "main" {
   max_size           = 10
   min_size           = 1
 
-  vpc_zone_identifier       = [local.private_subnet_ids]
+  vpc_zone_identifier       = local.private_subnet_ids
   target_group_arns  = [aws_lb_target_group.main.arn]
   health_check_type = "ELB"
 
